116a6ce2b48d2adb825eeeac46ffd4f5951360de
Wojtek Gorzawski <wojtek@c4w.site>
2024-12-02 20:34:47 +0100

    added registerClass funcion

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -7,15 +7,15 @@
 </template>
 
 <script setup lang="ts">
-import { $MyService } from '~/symbol';
-import type MyService from '~/services/myService';
-import type UserService from '~/services/userService';
+  import { $MyService } from '~/symbol';
+  import type MyService from '~/services/myService';
+  import type UserService from '~/services/userService';
 
-const userService = useContainer<UserService>('userService');
-const userName = userService.getUserName();
+  const userService = useContainer<UserService>('userService');
+  const userName = userService.getUserName();
 
-const container = useContainer();
-const myService = container.resolve<MyService>($MyService);
-const url = myService.getApiUrl();
-const user = myService.getUser();
+  const container = useContainer();
+  const myService = container.resolve<MyService>($MyService);
+  const url = myService.getApiUrl();
+  const user = myService.getUser();
 </script>
